2562ffa95d97d7355ccf44990585ceef1d610996
betelhem <betelhemkirub@gmail.com>
2023-08-07 23:23:25 +0300

    making hero responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <div className='relative z-10 bg-secondary-100'>
 
     <div className='flex w-11/12 mx-auto items-start justify-between pt-20  z-10 relative'>
-      <div className='w-full md:w-1/2 pt-8' >
+      <div className='w-full md:w-1/2  pt-8' >
         <h1 className='text-5xl md:flex justify-center mx-auto  md:text-left md:mx-0 text-center pt-5 leading-[3.2rem] xl:leading-[4.5rem] font-bold font-play  bg-gradient-to-r from-primary-300 via-primary-200 to-primary-100 text-transparent bg-clip-text md:text-5xl xl:text-6xl'>
           We have a wide variety of high quaity items to choose from </h1>
         <div className='mt-3 flex md:block justify-center font-kaisei'>
@@ -20,7 +20,7 @@ const Home = () => {
      </div>
      
       </div>
-      <div className="hidden md:block w-1/3 xl:w-full">
+      <div className="hidden md:block md:w-1/3 md:pt-10 xl:pt-5 ">
 
         <img className="" src={ Hero } alt="" />
       </div>
@@ -31,7 +31,7 @@ const Home = () => {
           
           <div className="flex justify-between gap-10 items-center ">
             <PiAirplaneLight size={60} className='rotate-90 text-primary-300'/>
-            <div className="flex flex-col lg:text-sm xl:text-xl">
+            <div className="flex flex-col lg:text-sm xl:text-base">
               <p className='font-bold text-primary-300 font-inter '>FREE SHIPPING</p>
               <p className='text-primary-100'>On order over 500 birr</p>
             </div>
@@ -42,7 +42,7 @@ const Home = () => {
           
           <div className="flex justify-between gap-10 items-center">
             <PiHeadset size={60} className='text-primary-300'/>
-            <div className="flex flex-col lg:text-sm xl:text-lg">
+            <div className="flex flex-col lg:text-sm xl:text-base">
               <p className='font-bold text-primary-300 font-inter'>24/7 CUSTUMER SERVICE</p>
               <p className='text-primary-100'>Call us 24/7 at 0966545645</p>
             </div>
@@ -53,7 +53,7 @@ const Home = () => {
           
           <div className="flex justify-between gap-10 items-center">
             <PiWallet size={60} className='text-primary-300'/>
-            <div className="flex flex-col lg:text-sm xl:text-lg">
+            <div className="flex flex-col lg:text-sm xl:text-base">
               <p className='font-bold  text-primary-300 font-inter'>CASH ON DELIVERY</p>
               <p className='text-primary-100'>100% money back guarantee</p>
             </div>
